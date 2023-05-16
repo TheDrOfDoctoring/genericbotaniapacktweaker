92a87ee97b6f8a5c35516468afd5471757278024
banstee <bennetanstee@gmail.com>
2023-05-16 17:21:49 +0400

    fix(?) imbuement mixin not working in pack itself

--- src/main/java/com/doctor/genericbotaniapacktweaker/mixin/ImbuementBlockMixin.java
+++ src/main/java/com/doctor/genericbotaniapacktweaker/mixin/ImbuementBlockMixin.java
@@ -1,6 +1,8 @@
 package com.doctor.genericbotaniapacktweaker.mixin;
 
+import com.doctor.genericbotaniapacktweaker.GenericBotaniaPackTweaker;
 import com.hollingsworth.arsnouveau.common.block.ImbuementBlock;
+import com.hollingsworth.arsnouveau.common.block.TickableModBlock;
 import com.hollingsworth.arsnouveau.common.block.tile.ImbuementTile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
@@ -9,12 +11,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ImbuementBlock.class)
-public abstract class ImbuementBlockMixin {
+public abstract class ImbuementBlockMixin extends TickableModBlock {
     //
     public boolean hasAnalogOutputSignal(BlockState p_49618_) {
         return true;
     }
     public int getAnalogOutputSignal(BlockState p_49620_, Level level, BlockPos pos) {
+        GenericBotaniaPackTweaker.LOGGER.info("test");
         ImbuementTile tile = (ImbuementTile) level.getBlockEntity(pos);
         return tile.getRecipeNow() != null ? 15 : 0;
     }
