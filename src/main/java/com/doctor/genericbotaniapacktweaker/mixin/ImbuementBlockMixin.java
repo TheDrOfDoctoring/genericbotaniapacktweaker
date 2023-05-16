0185b59d7ed32f0f2491498a1df5dc20c333f793
banstee <bennetanstee@gmail.com>
2023-05-16 18:46:32 +0400

    unnecessary stuff actually

--- src/main/java/com/doctor/genericbotaniapacktweaker/mixin/ImbuementBlockMixin.java
+++ src/main/java/com/doctor/genericbotaniapacktweaker/mixin/ImbuementBlockMixin.java
@@ -12,12 +12,11 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ImbuementBlock.class)
 public abstract class ImbuementBlockMixin extends TickableModBlock {
-    //
+    //Provide a comparator signal whilst a recipe is in progres for automation purposes (such as locking a hopper)
     public boolean hasAnalogOutputSignal(BlockState p_49618_) {
         return true;
     }
     public int getAnalogOutputSignal(BlockState p_49620_, Level level, BlockPos pos) {
-        GenericBotaniaPackTweaker.LOGGER.info("test");
         ImbuementTile tile = (ImbuementTile) level.getBlockEntity(pos);
         return tile.getRecipeNow() != null ? 15 : 0;
     }
