bd95b039f3cbf08e904f8212554ac79dda6f64f1
TheDrOfDoctoring <bennetanstee@gmail.com>
2023-05-29 09:42:26 +0400

    i should probably use datagen

--- src/main/java/com/doctor/genericbotaniapacktweaker/block/FastArcaneCore.java
+++ src/main/java/com/doctor/genericbotaniapacktweaker/block/FastArcaneCore.java
@@ -4,9 +4,11 @@ import com.doctor.genericbotaniapacktweaker.block.entity.FastArcaneCoreBlockEnti
 import com.hollingsworth.arsnouveau.common.block.ArcaneCore;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class FastArcaneCore extends ArcaneCore {
+
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new FastArcaneCoreBlockEntity(pos, state);
